f4f2b7eb120ff1c79980d01209d0bdd3dc3dc134
paolo-della-fera <paolodf2005@gmail.com>
2026-02-10 16:55:58 +0100

    completed the fourth exercise

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -2,12 +2,57 @@
 
 const word = 'javascript';
 
+// creo un array conteneti le volcali che mi servirà per il confronto
+const vocals = 'aeiou'
 
 // Dichiara la funzione qui.
+/* 
+function countVocals(voc) {
+    // creo una variabile dove salvare quante volte trovo una vocale 
+    let vocalCount = 0
+    // creo un array dove salvere le vocali trovate
+    let foundVocals = []
+    // uso il ciclo for per analizzare oni lettera della parola
+    for (let i = 0; i < voc.length; i++) {
+        let char = voc[i]
+        // SE la lettera è contenuta nella lista di vocali
+        if (vocals.indexOf(char) !== -1) {
+            // aumeta il numero di vocali contenute nella parola 
+            vocalCount++
+            // e si aggiunge all'array che contine tutte le vocali trovate 
+            foundVocals.push(char)
+        }
+    }
+    return vocalCount + ' (' + foundVocals.join(', ') + ')';
+} 
+*/
 
+
+// arrow function 
+const countVocals = (voc) => {
+    // creo una variabile dove salvare quante volte trovo una vocale 
+    let vocalCount = 0
+    // creo un array dove salvere le vocali trovate
+    let foundVocals = []
+    // uso il ciclo for per analizzare oni lettera della parola
+    for (let i = 0; i < voc.length; i++) {
+        let char = voc[i]
+        // SE la lettera è contenuta nella lista di vocali
+        if (vocals.indexOf(char) !== -1) {
+            // aumeta il numero di vocali contenute nella parola 
+            vocalCount++
+            // e si aggiunge all'array che contine tutte le vocali trovate 
+            foundVocals.push(char)
+        }
+    }
+    return vocalCount + ' (' + foundVocals.join(', ') + ')';
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+// mi salvo il return in una viariabile 
+const result = countVocals(word)
+// stampo il risultato in console
+console.log(result);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
