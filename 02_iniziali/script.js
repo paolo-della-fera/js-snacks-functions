d0728aef1ea162a1cadb84549bcd8824f0227a29
paolo-della-fera <paolodf2005@gmail.com>
2026-02-10 15:50:45 +0100

    completed the second exercise

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,11 +2,33 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
+/* const firstLetter = []; */
 
 // Dichiara la funzione qui.
+/* 
+function takeFirstLetter(arr) {
+    // creo un ciclo for per prendere la prima lettera
+    for (let i = 0; i < arr.length; i++) {
+        // aggiungo la prima lettere di ogni parola nell nuovo array creato precedentemente
+        firstLetter.push(arr[i][0])
+    }
+    return firstLetter
+} 
+*/
 
+// arrow function
+const takeFirstLetter = (arr) => {
+    const firstLetter = [];
+    for (let i = 0; i < arr.length; i++) {
+        // aggiungo la prima lettere di ogni parola nell nuovo array creato precedentemente
+        firstLetter.push(arr[i][0])
+    }
+    return firstLetter
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+// mi stampo in console l'array con solo le iniziali delle parole del primo array 
+console.log(takeFirstLetter(names));
 
 
 
