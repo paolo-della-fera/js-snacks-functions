fa29ae554129623a19ddc2512f149afc9a5b1326
paolo-della-fera <paolodf2005@gmail.com>
2026-02-10 17:52:31 +0100

    complete the bonus exercise

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -8,11 +8,53 @@ e buonasera se è sera (oltre le 17)
 const name = 'Mario';
 
 
-// Dichiara la funzione qui.
+// Dichiara la funzione qui. 
+/* 
+function saluta(name) {
+    // creo la costante per ottenere l'ora attuale
+    const hour = new Date().getHours();
+    let message = ''
+    // SE è mattino (fino alle 13)
+    if (hour < 13) {
+        message = 'Buongiorno'
+    }
+    // ALTRIMENTI SE è pomeriggio (fino alle 17)
+    else if (hour < 17) {
+        message = 'Buon Pomeriggio'
+    }
+    // ALTRIMENTI è notte (oltre le 17)
+    else {
+        message = 'Buonasera'
+    }
+    return message + ' ' + name
+} 
+*/
 
+
+// arrow function
+const saluta = (name) => {
+    // creo la costante per ottenere l'ora attuale
+    const hour = new Date().getHours();
+    let message = ''
+    // SE è mattino (fino alle 13)
+    if (hour < 13) {
+        message = 'Buongiorno'
+    }
+    // ALTRIMENTI SE è pomeriggio (fino alle 17)
+    else if (hour < 17) {
+        message = 'Buon Pomeriggio'
+    }
+    // ALTRIMENTI è notte (oltre le 17)
+    else {
+        message = 'Buonasera'
+    }
+    return message + ' ' + name
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+// mi salvo il risultato in una variabile
+const result = saluta(name)
+// lo stampo in console
+console.log(result);
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
