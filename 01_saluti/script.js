b868fd70c07866922aa27865077fff70af0a9b7f
paolo-della-fera <paolodf2005@gmail.com>
2026-02-10 15:25:16 +0100

    completed the first exercise

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -5,9 +5,23 @@ const userName = 'Mario';
 
 // Dichiara la funzione qui.
 
+// creo la funzione che mi restituisce il saluto seguito dal nome 
+/* 
+function saluta(userName) {
+    return 'Ciao ' + userName
+} 
+*/
+
+// arrow function
+const saluta = (userName) => 'ciao ' + userName
+
 
 // Invoca la funzione qui e stampa il risultato in console
+/* 
+console.log(saluta(userName)); 
+*/
 
-
+// stampo il risultato in console della arrow function 
+console.log(saluta(userName));
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
